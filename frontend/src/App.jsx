6430624c8fb4c2d146e090bd49432df7f3f09ff1
AlikoGrace <gracealiko09@gmail.com>
2025-12-01 23:06:16 +0000

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Expense from "./pages/Dashboard/Expense";
+import Income from "./pages/Dashboard/Income";
+
 function App() {
-  return <div className="text-3xl text-purple-300"> App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/income" element={<Income />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
